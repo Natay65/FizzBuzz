fb838552e8b6fb62f90faa6f6f5d23b89ec63987
nathan.tayar <nathan.tayar@gmail.com>
2021-03-18 15:53:26 +0200

    fizzbuzz seven modification

--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -26,6 +26,6 @@ public class TestFizzBuzz {
 
     @Test
     public void testWithSeven() {
-        Assert.assertEquals("", fizzBuzz.check(7));
+        Assert.assertEquals("Seven", fizzBuzz.check(7));
     }
 }
